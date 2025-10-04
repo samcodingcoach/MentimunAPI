d294fd41aba867300a96d7bcfa14df2224802a02
Samsu Bahri <samsu.bahri@binus.ac.id>
2025-10-04 21:22:34 +0800

    detail order add

---
+++ api/dapur/detail_order.php
@@ -0,0 +1,46 @@
+<?php
+header('Content-Type: application/json');
+error_reporting(E_ALL & ~E_NOTICE);
+
+include "../../config/koneksi.php";
+$id = $_GET['id_order'];
+
+
+$sql = "SELECT
+	dapur_order_detail.id_order, 
+	dapur_order_detail.id_pesanan_detail, 
+	view_produk2.kode_produk, 
+	view_produk2.nama_produk, 
+	view_produk2.nama_kategori, 
+	dapur_order_detail.ready, 
+	dapur_order_detail.tgl_update, 
+	dapur_order_detail.waktu_batal, 
+	dapur_order_detail.waktu_ready, 
+	dapur_order_detail.waktu_delivered, 
+	pesanan_detail.ta_dinein, 
+	pesanan_detail.qty
+FROM
+	dapur_order_detail
+	INNER JOIN
+	pesanan_detail
+	ON 
+		dapur_order_detail.id_pesanan_detail = pesanan_detail.id_pesanan_detail
+	INNER JOIN
+	view_produk2
+	ON 
+		pesanan_detail.id_produk_sell = view_produk2.id_produk_sell
+    where id_order = $id
+    ";
+
+$result = mysqli_query($conn, $sql);
+$data = [];
+
+if (mysqli_num_rows($result) > 0) {
+	while($row = mysqli_fetch_assoc($result)) {
+		$data[] = $row;
+	}
+}
+
+echo json_encode($data);
+mysqli_close($conn);
+?>
