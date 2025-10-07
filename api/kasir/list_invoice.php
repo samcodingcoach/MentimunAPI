256d59c6d26405ee3d89432a3894a0dd65f9e15a
Samsu Bahri <samsu.bahri@binus.ac.id>
2025-10-07 21:29:47 +0800

    fix

--- api/kasir/list_invoice.php
+++ api/kasir/list_invoice.php
@@ -5,12 +5,13 @@ error_reporting(E_ALL & ~E_NOTICE);
 include "../../config/koneksi.php";
 
 $sql = "SELECT
+	proses_pembayaran.id_pesanan, 
 	proses_pembayaran.kode_payment, 
 	DATE_FORMAT(proses_pembayaran.tanggal_payment,'%H:%i') AS tgl, 
 	konsumen.nama_konsumen, 
 	pesanan.id_meja, 
 	proses_pembayaran.id_tagihan, 
-	view_invoice.total_dengan_ppn as total_cart
+	SUM(view_invoice.total_dengan_ppn) AS total_cart
 FROM
 	proses_pembayaran
 	INNER JOIN
@@ -24,11 +25,15 @@ FROM
 	INNER JOIN
 	view_invoice
 	ON 
-		pesanan.id_pesanan = view_invoice.id_pesanan
+		proses_pembayaran.id_pesanan = view_invoice.id_pesanan
 WHERE
 	DATE(tanggal_payment) = CURDATE() AND
 	proses_pembayaran.id_tagihan IS NOT NULL AND
 	proses_pembayaran.`status` = 0
+  
+  GROUP BY pesanan.id_pesanan
+  
+  
 ";
 
 $result = mysqli_query($conn, $sql);
